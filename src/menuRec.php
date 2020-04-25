82ede7dc7f0b66c60188e6c34abe7d57560ce6b7
Ilya Ezelev <iiezelev@gmail.com>
2020-04-25 20:11:53 +0300

    Update menuRec.php

--- src/menuRec.php
+++ src/menuRec.php
@@ -1,1 +1,14 @@
 /* TODO recursive bitrix tree menu */
+<?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');?>
+
+<?
+
+CModule::IncludeModule("iblock");
+$iblockId = "xxx";
+
+$arFilter = Array('IBLOCK_ID'=>$iblockId);
+$db_list = CIBlockSection::GetList(Array($by=>$order), $arFilter, true);
+while($arSect = $db_list->GetNext())
+{
+    // echo $arSect['ID'].' '.$arSect['NAME'].': '.$arSect['ELEMENT_CNT'].'<br>';
+}
